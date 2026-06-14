d32c6f3d2e9141d2c9f54b444fca18bb76d5a465
explorer369 <zzf198987@qq.com>
2026-06-14 21:51:24 +0800

    配置 GitHub Pages 自动部署 + vite base 路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  base: "/english-learning-app/",
   plugins: [svelte({ hot: !process.env.VITEST })],
   test: {
     globals: true,
